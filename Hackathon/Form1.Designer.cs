c75a54e7c431f45b398c65864ebd7f626ffd96a5
Dohn Michael B. Varquez <23-1-02140@vsu.edu.ph>
2025-02-06 10:54:05 +0800

    fix

--- Hackathon/Form1.Designer.cs
+++ Hackathon/Form1.Designer.cs
@@ -30,7 +30,13 @@
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2Button_aboutus = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button_foodhub = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button_home = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2Button4 = new Guna.UI2.WinForms.Guna2Button();
             this.tableLayoutPanel1.SuspendLayout();
+            this.guna2Panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -38,38 +44,130 @@
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.Controls.Add(this.guna2Panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.guna2Panel2, 0, 1);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(-6, -2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.257477F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 91.74252F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.68412F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 89.31587F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(2560, 1538);
             this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
             // 
             // guna2Panel1
             // 
-            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2Panel1.Controls.Add(this.guna2Button4);
+            this.guna2Panel1.Controls.Add(this.guna2Button_aboutus);
+            this.guna2Panel1.Controls.Add(this.guna2Button_foodhub);
+            this.guna2Panel1.Controls.Add(this.guna2Button_home);
             this.guna2Panel1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(62)))), ((int)(((byte)(25)))));
             this.guna2Panel1.Location = new System.Drawing.Point(3, 3);
             this.guna2Panel1.Name = "guna2Panel1";
             this.guna2Panel1.ShadowDecoration.Parent = this.guna2Panel1;
-            this.guna2Panel1.Size = new System.Drawing.Size(2554, 120);
+            this.guna2Panel1.Size = new System.Drawing.Size(2554, 158);
             this.guna2Panel1.TabIndex = 0;
+            // 
+            // guna2Button_aboutus
+            // 
+            this.guna2Button_aboutus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(62)))), ((int)(((byte)(25)))));
+            this.guna2Button_aboutus.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(206)))), ((int)(((byte)(95)))));
+            this.guna2Button_aboutus.BorderRadius = 10;
+            this.guna2Button_aboutus.BorderThickness = 2;
+            this.guna2Button_aboutus.CheckedState.Parent = this.guna2Button_aboutus;
+            this.guna2Button_aboutus.CustomImages.Parent = this.guna2Button_aboutus;
+            this.guna2Button_aboutus.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(62)))), ((int)(((byte)(25)))));
+            this.guna2Button_aboutus.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button_aboutus.ForeColor = System.Drawing.Color.White;
+            this.guna2Button_aboutus.HoverState.Parent = this.guna2Button_aboutus;
+            this.guna2Button_aboutus.Location = new System.Drawing.Point(1853, 32);
+            this.guna2Button_aboutus.Name = "guna2Button_aboutus";
+            this.guna2Button_aboutus.ShadowDecoration.Parent = this.guna2Button_aboutus;
+            this.guna2Button_aboutus.Size = new System.Drawing.Size(217, 88);
+            this.guna2Button_aboutus.TabIndex = 2;
+            this.guna2Button_aboutus.Text = "ABOUT US";
+            // 
+            // guna2Button_foodhub
+            // 
+            this.guna2Button_foodhub.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(62)))), ((int)(((byte)(25)))));
+            this.guna2Button_foodhub.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(206)))), ((int)(((byte)(95)))));
+            this.guna2Button_foodhub.BorderRadius = 10;
+            this.guna2Button_foodhub.BorderThickness = 2;
+            this.guna2Button_foodhub.CheckedState.Parent = this.guna2Button_foodhub;
+            this.guna2Button_foodhub.CustomImages.Parent = this.guna2Button_foodhub;
+            this.guna2Button_foodhub.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(62)))), ((int)(((byte)(25)))));
+            this.guna2Button_foodhub.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button_foodhub.ForeColor = System.Drawing.Color.White;
+            this.guna2Button_foodhub.HoverState.Parent = this.guna2Button_foodhub;
+            this.guna2Button_foodhub.Location = new System.Drawing.Point(1566, 32);
+            this.guna2Button_foodhub.Name = "guna2Button_foodhub";
+            this.guna2Button_foodhub.ShadowDecoration.Parent = this.guna2Button_foodhub;
+            this.guna2Button_foodhub.Size = new System.Drawing.Size(217, 88);
+            this.guna2Button_foodhub.TabIndex = 1;
+            this.guna2Button_foodhub.Text = "FOOD HUB";
+            // 
+            // guna2Button_home
+            // 
+            this.guna2Button_home.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(62)))), ((int)(((byte)(25)))));
+            this.guna2Button_home.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(206)))), ((int)(((byte)(95)))));
+            this.guna2Button_home.BorderRadius = 10;
+            this.guna2Button_home.BorderThickness = 2;
+            this.guna2Button_home.CheckedState.Parent = this.guna2Button_home;
+            this.guna2Button_home.CustomImages.Parent = this.guna2Button_home;
+            this.guna2Button_home.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(62)))), ((int)(((byte)(25)))));
+            this.guna2Button_home.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button_home.ForeColor = System.Drawing.Color.White;
+            this.guna2Button_home.HoverState.Parent = this.guna2Button_home;
+            this.guna2Button_home.Location = new System.Drawing.Point(1283, 32);
+            this.guna2Button_home.Name = "guna2Button_home";
+            this.guna2Button_home.ShadowDecoration.Parent = this.guna2Button_home;
+            this.guna2Button_home.Size = new System.Drawing.Size(217, 88);
+            this.guna2Button_home.TabIndex = 0;
+            this.guna2Button_home.Text = "HOME";
+            // 
+            // guna2Panel2
+            // 
+            this.guna2Panel2.AutoScroll = true;
+            this.guna2Panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.guna2Panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.guna2Panel2.Location = new System.Drawing.Point(3, 167);
+            this.guna2Panel2.Name = "guna2Panel2";
+            this.guna2Panel2.ShadowDecoration.Parent = this.guna2Panel2;
+            this.guna2Panel2.Size = new System.Drawing.Size(2554, 1368);
+            this.guna2Panel2.TabIndex = 1;
+            // 
+            // guna2Button4
+            // 
+            this.guna2Button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(62)))), ((int)(((byte)(25)))));
+            this.guna2Button4.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(206)))), ((int)(((byte)(95)))));
+            this.guna2Button4.BorderRadius = 10;
+            this.guna2Button4.BorderThickness = 2;
+            this.guna2Button4.CheckedState.Parent = this.guna2Button4;
+            this.guna2Button4.CustomImages.Parent = this.guna2Button4;
+            this.guna2Button4.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(206)))), ((int)(((byte)(95)))));
+            this.guna2Button4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(62)))), ((int)(((byte)(35)))));
+            this.guna2Button4.HoverState.Parent = this.guna2Button4;
+            this.guna2Button4.Location = new System.Drawing.Point(2235, 32);
+            this.guna2Button4.Name = "guna2Button4";
+            this.guna2Button4.ShadowDecoration.Parent = this.guna2Button4;
+            this.guna2Button4.Size = new System.Drawing.Size(217, 88);
+            this.guna2Button4.TabIndex = 3;
+            this.guna2Button4.Text = "DONATE";
             // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(2536, 1536);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Home";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
+            this.guna2Panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -78,6 +176,11 @@
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button_home;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private Guna.UI2.WinForms.Guna2Button guna2Button_aboutus;
+        private Guna.UI2.WinForms.Guna2Button guna2Button_foodhub;
+        private Guna.UI2.WinForms.Guna2Button guna2Button4;
     }
 }
 
