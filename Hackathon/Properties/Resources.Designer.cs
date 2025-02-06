02ce22b4a97bc8850e75d18639431401cb355e1c
Dohn Michael B. Varquez <23-1-02140@vsu.edu.ph>
2025-02-06 11:45:09 +0800

    Added Home View

--- Hackathon/Properties/Resources.Designer.cs
+++ Hackathon/Properties/Resources.Designer.cs
@@ -69,5 +69,25 @@ namespace Hackathon.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _21 {
+            get {
+                object obj = ResourceManager.GetObject("21", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap hand {
+            get {
+                object obj = ResourceManager.GetObject("hand", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
